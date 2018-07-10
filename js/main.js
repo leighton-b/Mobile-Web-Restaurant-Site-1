c4fd8ce94d533ccc983421559438a47bb70c5111
Leighton B <lbyfield2015@live.com>
2018-07-10 10:59:33 -0400

    Accessability changes

--- js/main.js
+++ js/main.js
@@ -165,7 +165,7 @@ createRestaurantHTML = (restaurant) => {
   image.setAttribute('alt', `Image of ${restaurant.name} restaurant`);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
